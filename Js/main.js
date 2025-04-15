fa8b1870ed35a93c6e79e9959cba3978cba2880c
LENOVO <nandikakaweesha4005@gmail.com>
2025-04-15 09:35:29 +0530

    main.js part

--- Js/main.js
+++ Js/main.js
@@ -1,6 +1,26 @@
-const scrollRevealOption={
+const scrollRevealOption = {
     distance: "50px",
     origin: "bottom",
     duration: 1000,
-
 };
+
+const tabList = document.querySelector(".resume__tablist");
+tabList.addEventListener("click", (e) => {
+    const tabIndex = e.target.dataset.tab;
+    if (!tabIndex) return; // Exit if no tab is clicked
+
+    const tabs = document.querySelectorAll("[data-tab]");
+    tabs.forEach((tab) => {
+        tab.classList.toggle("active", tab.dataset.tab === tabIndex);
+    });
+
+    const activePanel = document.querySelector(".panel__grid.active");
+    const toActivePanel = document.querySelector(`[data-panel="${tabIndex}"]`);
+    if (activePanel.dataset.panel === tabIndex) return;
+
+    activePanel.classList.add("close");
+    activePanel.addEventListener("animationend", () => {
+        activePanel.classList.remove("active", "close");
+        toActivePanel.classList.add("active");
+    }, { once: true });
+});
