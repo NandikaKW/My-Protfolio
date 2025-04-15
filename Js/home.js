5fe06a364c3f959023b98b451d5793717cff5dbf
LENOVO <nandikakaweesha4005@gmail.com>
2025-04-15 09:49:52 +0530

    home.js part

---
+++ Js/home.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const menuBtn = document.getElementById("menu-btn");
+    const navLinks = document.getElementById("nav-links");
+    const menuBtnIcon = menuBtn.querySelector("i");
+
+
+});
