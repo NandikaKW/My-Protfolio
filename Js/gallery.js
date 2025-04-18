68613f2bfcc940587a1d5b9dc1b4c02702978a64
LENOVO <nandikakaweesha4005@gmail.com>
2025-04-18 11:05:36 +0530

    new commit

--- Js/gallery.js
+++ Js/gallery.js
@@ -3,11 +3,11 @@ const divs = galleryContainer.querySelectorAll('.gallery-wrapper div');
 const fullscreenView = galleryContainer.querySelector('.image-view');
 const cancelBtn = galleryContainer.querySelector('#cancel-btn');
 
-divs.forEach(function (BG, idx) {
+divs.forEach(function (BG) {
     BG.addEventListener('click', function () {
+        const img = BG.getAttribute('data-image');
         fullscreenView.style.display = "block";
-        fullscreenView.style.background = `url(assets/images/image_${idx + 1}.jpg) center/cover no-repeat`;
-
+        fullscreenView.style.background = `url(assets/images/${img}) center/cover no-repeat`;
     });
 });
 
