000cb3f2bdc6e52883f39ca8871d22917c7188bc
LENOVO <nandikakaweesha4005@gmail.com>
2025-04-28 09:08:29 +0530

    smoothScrollNav js

---
+++ Js/smoothScrollNav.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded',()=>{
+    if (window.matchMedia('min-width:1440px').matches){
+        const navLinks=document.querySelectorAll('.right-side-nav .nav-link');
+
+    }
+
+});
